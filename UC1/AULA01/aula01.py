1b887ae9df431c6aebb36b3ff2c2eaf8ec8c00ba
THALYTA DE SOUSA MARTINS <thalyta.martins@aluno.senac.rio>
2026-08-17 21:05:43 -0300

    meu primeiro versionamento

---
+++ UC1/AULA01/aula01.py
@@ -0,0 +1,50 @@
+print("olámundo!")
+nome = "maria" 
+idade = 30 
+preco = 19.99
+esta_matriculada = True
+
+notas = [8.0, 7.5]
+aluno  = ("maria" , 30)
+disciplinas = ("python", "lógica")
+ 
+cadastro = {"nome": "maria", "idade": 30}
+print(type("maria"))
+print(type(30))
+print(type(19.99))
+
+nota_1 = 2
+nota_2 = 3
+x = 15
+y = 20
+print("x é maior que y?", x>y)
+print("x é igual a y?", x==y)
+resposta = x>y
+print(resposta)
+print(type)
+
+cnh = True
+bebidinha = False
+posso_dirigir = cnh and not bebidinha
+print(posso_dirigir)
+
+Ônibus = False
+trêm = False
+venho_para_aula = Ônibus or trêm
+print("venho para aula?", venho_para_aula)
+
+locomocao  = input(diga qual sua locomocao:")
+choveu = True
+
+if choveu and locomocao== 'moto':
+      resultado = "Tô todo molhado :("
+elif not choveu and locomocao=='moto':
+else:
+      resultado = "Tô seco :)"
+
+    
+
+print(resultado)
+
+
+
