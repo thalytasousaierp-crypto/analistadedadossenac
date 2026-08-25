bc78c31f5e0dce98d946df0194e17827ce81d00f
thalytasousaierp-crypto <thalytasousa.ierp@gmail.com>
2026-08-24 21:56:11 -0300

    Add files via upload

---
+++ UC1/AULA01/desafio2.py
@@ -0,0 +1,17 @@
+nota1 = 4 
+nota2 = 6
+nota3 = 8
+nota4 = 7
+
+media (nota1 + nota2 + nota3 + nota4) /4
+print ("media", media)
+
+    if media >= 7:
+        print ("aprovado")
+
+    elif media >= 5:
+        print ("recuperacao")
+        
+    else: print("reprovado")
+        print("resultado", media)
+    
