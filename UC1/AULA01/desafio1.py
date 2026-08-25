4e0dd1be991959b6693f85b6d704a6c3d9227c1b
THALYTA DE SOUSA MARTINS <thalyta.martins@aluno.senac.rio>
2026-08-24 21:49:49 -0300

    aula06

---
+++ UC1/AULA01/desafio1.py
@@ -0,0 +1,21 @@
+# numero1 = 20
+# numero2 = 25
+# numero3 = 35
+
+#     if numero1 < numero2 and numero2 < numero3:
+#         print (numero1, numero2, numero3)
+    
+
+numero1 = 20
+numero2 = 10
+numero3 = 5
+
+if numero1 < numero2 and numero2 < numero3:
+        print ("numero1, numero2, numero3")
+        
+      
+      
+      
+      
+      
+        
