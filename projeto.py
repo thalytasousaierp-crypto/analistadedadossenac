02265953085af3c3e33819b46e28ac32cc9991c7
thalytasousaierp-crypto <thalytasousa.ierp@gmail.com>
2026-08-31 21:28:42 -0300

    Add files via upload

---
+++ projeto.py
@@ -0,0 +1,16 @@
+mesas = {
+    1: "Livre",
+    2: "ocupada",
+    3: "Ocupada" ,    
+    4: "Ocupada",
+    5: "Livre"     
+}
+
+print("alterar status da mesa")
+print("mesas2:", mesas[2])
+
+mesas[2] = "Desistente"
+print(mesas)
+{1: 'livre', 2: 'livre', 3: 'ocupada', 4:'ocupada', 5: 'livre' }
+
+
