20c1f6cf557c1efcaaf59dbac432eacad35b2fe3
thalytasousaierp-crypto <thalytasousa.ierp@gmail.com>
2026-09-18 21:25:15 -0300

    Add files via upload

---
+++ treinamentos.produtos.sql
@@ -0,0 +1,14 @@
+SELECT * FROM meu_ecommerce.produtos;
+SET GLOBAL local_infile = 1; -- marcação de aceite para arquivos locais (passo extra 01 junto ao load data)
+
+-- 'OPT_LOCAL_INFILE=1' -- (passo extra 02 junto ao load data) inserir na sua conexão local (edit da conexão >> Advanced >> Others)
+
+LOAD DATA LOCAL INFILE 'C:/Users/nomenamaquina/caminhodiretorio/vendas_produtos.csv' -- Ajuste o caminho no seu banco local
+INTO TABLE vendas_online.produtos
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n' -- Aqui: CR LF
+IGNORE 1 ROWS -- Pula o cabeçalho 'id_produto,nome...'
+(id_produto, nome, categoria, @preco_var, estoque) -- Mapeia colunas
+SET preco = REPLACE(@preco_var, '.', '.'); -- Garante que o decimal seja lido corretamente
+
